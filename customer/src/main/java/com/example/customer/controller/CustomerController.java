a35ce5a773c65c1aa3cfe0f0af97e59316e108ea
James Spargo <solacedforever@gmail.com>
2017-09-25 14:54:08 -0400

    finished for now

--- customer/src/main/java/com/example/customer/controller/CustomerController.java
+++ customer/src/main/java/com/example/customer/controller/CustomerController.java
@@ -7,10 +7,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -58,6 +55,12 @@ public class CustomerController {
     return "administration";
   }
 
+  @GetMapping("/customer/{customerId}")
+  String viewCustomerDetails(@PathVariable String customerId, Model model) {
+    Customer aCustomer = customerService.getById(Integer.parseInt(customerId));
+    model.addAttribute("customer",aCustomer);
+    return "customer_details";
+  }
 
   @ExceptionHandler(value = Exception.class)
   public String handleDefaultErrors(final Exception exception, Model model) {
